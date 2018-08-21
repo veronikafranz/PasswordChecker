2ed444b56a5c7e174b248686e524921d48885749
veron <veron@LAPTOP-01B6OD61>
2018-08-21 19:43:38 +0200

    password checker incl JUnit test file

---
+++ src/PasswortCheckerTest.java
@@ -0,0 +1,35 @@
+import junit.framework.TestCase;
+
+public class PasswortCheckerTest extends TestCase {
+
+	PasswordChecker pwc;
+	
+	public void setUp() {
+		pwc = new PasswordChecker();
+	}
+	
+	public void testeMitGrossuchstabeUndMitZahl() {
+		assertEquals("Prüfung bei Großbuchstabe und Zahl fehlerhaft.", true, pwc.checkPassword("fhghjr9H"));
+	}
+	
+	public void testeMitGrossuchstabeUndOhneZahl() {
+		assertEquals("Prüfung mit Großbuchstabe und ohne Zahl fehlerhaft.", false, pwc.checkPassword("fhHhjr"));
+	}
+	
+	public void testeOhneGrossbuchstabeUndMitZahl() {
+		assertEquals("Prüfung ohne Großbuchstabe und mit Zahl fehlerhaft.", false, pwc.checkPassword("fhghjr9"));
+	}
+	
+	public void testeOhneGrossuchstabeUndOhneZahl() {
+		assertEquals("Prüfung ohne Großbuchstabe und ohne Zahl fehlerhaft.", false, pwc.checkPassword("fhghjr!"));
+	}
+	
+	public void testeOhneKleinbuchstabe() {
+		assertEquals("Prüfung ohne Kleinbuchstabe fehlerhaft.", false, pwc.checkPassword("FHOFDB89"));
+	}
+	
+	public void testeMitLeerzeichen() {
+		assertEquals("Prüfung mit Leerzeichen fehlerhaft.", false, pwc.checkPassword("fHJ8hg hjr!"));
+	}
+}
+
